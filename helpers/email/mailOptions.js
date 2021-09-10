4d1ccd3537714ad6705f960ea389bbe74a8d921a
Ioanna-Mitsani <13-13099@saeinstitute.edu>
2021-09-10 19:03:59 +0300

    Fixed partially nodemailer  email content format

--- helpers/email/mailOptions.js
+++ helpers/email/mailOptions.js
@@ -4,16 +4,12 @@ const registrationEmail = (email, firstName, verificationLink) => {
         from: '"Pharma App" nodejsauthmailer@gmail.com',
         to: email,
         subject: 'Pharma App | Verify your account',
-        text: `Welcome to Pharma!     
+        text:`
+        Welcome to Pharma!
+             
         Hello ${firstName},
         Welcome to Pharma app! To verify your e-mail address, please click the following link:
-        ${verificationLink}`,
-        html: `<body>
-               <h3>Welcome to Pharma!</h3>     
-                <p>Hello ${firstName},</p>
-                <p>Welcome to Pharma app! To verify your e-mail address, please click
-                <a href="${verificationLink}">here</a></p>
-                </body>`
+        ${verificationLink}`
     }
     return content;
 }
@@ -25,12 +21,11 @@ const resetPasswordEmail = (email, firstName, verificationLink) => {
         to: email,
         subject: 'Pharma App | Password reset',
         text:`
-            Hello, ${firstName},
-
-            To reset your password for the Pharma App, click the following link:${verificationLink}
+        Hello, ${firstName},
+        To reset your password for the Pharma App, click the following link:${verificationLink}
             
-            If you did not forget your password, please ignore this e-mail.
-            `
+        If you did not forget your password, please ignore this e-mail.
+        `
             
     }
     return content;
